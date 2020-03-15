9404d4f8909be6b0afbd0deddbcbe2d4abac0d64
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-03-15 14:31:44 +0400

    feat: add set color of active menu item

--- scrypt.js
+++ scrypt.js
@@ -31,97 +31,148 @@ onload = function () {
                 style.border = '5px solid #F06C64';
             }
 
-            //console.log(onClickElement.classList[0]);
+            //Click on a arrow to slide
             if (onClickElement.classList[0] === 'arrow') {
                 slide(onClickElement.classList[1]);
             }
 
-        }
+            //Click on a menu item to change color
+            if (onClickElement.classList[0] === 'navigation-menu') {
+                document.querySelectorAll('.nav-main').forEach(item => {
+                    item.style.color = '#ffffff';
+                });
 
-        //Move slide to the left or to the right
-        function slide(arrowDirection) {
-            let direction;
-            let toLeft;
+                onClickElement.firstElementChild.style.color = '#f06c64';
+            }
 
-            arrowDirection === 'arrow-left' ? (
-                direction = -1,
-                toLeft = true
-            )
-                : (
-                    direction = 1,
-                    toLeft = false
-                );
+            console.log(onClickElement);
+            if (onClickElement.classList[0] === 'menu-item') {
+                debugger;
+                document.querySelectorAll('.menu-item').forEach(item => {
+                    item.style.color = '#767e9e';
+                });
 
-            setSliderLeftOffset(toLeft);
-
-            let interval = setInterval(function () {
-                document.querySelectorAll('.slider-wrapper').forEach((item) => {
-                    left += direction;
-                    item.style.left = `${left}px`;
-                    if (countSlider > 1018) {
-                        countSlider = 0;
-                        clearInterval(interval);
-                        return;
-                    }
-                    countSlider++;
-                })
-            });
-        }
-
-        function setSliderLeftOffset(toLeft) {
-            debugger;
-            document.querySelectorAll('.slider-wrapper').forEach((item) => {
-                if ((item.offsetLeft > 1018) && !toLeft) {
-                    item.offsetLeft
-                };
-            });
-        }
-
-        function setSlidePosition(toLeft) {
-            let slide = getLeftmostOrRightmostSlide(toLeft);
-        }
-
-        //Function returns leftmost or rightmost slide
-        function getLeftmostOrRightmostSlide(leftmost) {
-            let slideToSearch;
-            let left;
-            document.querySelectorAll('.slider-wrapper').forEach(slide => {
-                let styleLeft = slide.offsetLeft;
-
-                if (left == undefined) {
-                    slideToSearch = slide;
-                    left = slide.offsetLeft;
-                }
-
-                if (leftmost) {
-                    //Get min left of sliders
-                    if (styleLeft < left) {
-                        left = styleLeft;
-                        slideToSearch = slide;
-                    }
-                }
-                else {
-                    //Get max left of sliders
-                    if (styleLeft > left) {
-                        left = styleLeft;
-                        slideToSearch = slide;
-                    }
-                }
-            })
-
-            return slideToSearch;
-        }
-
-        //Function parse string of pixels to number
-        function leftStyleParse(value) {
-
-            return value === undefined ?
-                undefined :
-                Number.parseInt(value.substring(0, value.length - 2));
-        }
+                onClickElement.style.color = '#dedede';
+            }
+        }        
     };
 }
 
+function AllActive() {    
+    SetColor('#all-menu');
+}
+
+function WebDesignActive() {    
+    SetColor('#web-design-menu');
+}
+
+function GraphicActive() {    
+    SetColor('#graphic-design-menu');
+}
+
+function ArtworkActive() {    
+    SetColor('#artwork-menu');
+}
+
+//Function set color and border-color of the portfolio menu
+function SetColor(itemName) {
+    let item = document.querySelector(itemName);
+
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.style.color = '#767e9e';
+        // item.style.borderColor = '#767e9e';
+
+    });
+    item.style.color = '#dedede';
+    // item.style.borderColor = '#dedede';
+}
+
+//Move slide to the left or to the right
+function slide(arrowDirection) {
+    let direction;
+    let toLeft;
+
+    arrowDirection === 'arrow-left' ? (
+        direction = -1,
+        toLeft = true
+    )
+        : (
+            direction = 1,
+            toLeft = false
+        );
+
+    setSliderLeftOffset(toLeft);
+
+    let interval = setInterval(function () {
+        document.querySelectorAll('.slider-wrapper').forEach((item) => {
+            left += direction;
+            item.style.left = `${left}px`;
+            if (countSlider > 1018) {
+                countSlider = 0;
+                clearInterval(interval);
+                return;
+            }
+            countSlider++;
+        })
+    });
+}
+
+
+function setSliderLeftOffset(toLeft) {
+    debugger;
+    document.querySelectorAll('.slider-wrapper').forEach((item) => {
+        if ((item.offsetLeft > 1018) && !toLeft) {
+            item.offsetLeft
+        };
+    });
+}
+
+function setSlidePosition(toLeft) {
+    let slide = getLeftmostOrRightmostSlide(toLeft);
+}
+
+//Function returns leftmost or rightmost slide
+function getLeftmostOrRightmostSlide(leftmost) {
+    let slideToSearch;
+    let left;
+    document.querySelectorAll('.slider-wrapper').forEach(slide => {
+        let styleLeft = slide.offsetLeft;
+
+        if (left == undefined) {
+            slideToSearch = slide;
+            left = slide.offsetLeft;
+        }
+
+        if (leftmost) {
+            //Get min left of sliders
+            if (styleLeft < left) {
+                left = styleLeft;
+                slideToSearch = slide;
+            }
+        }
+        else {
+            //Get max left of sliders
+            if (styleLeft > left) {
+                left = styleLeft;
+                slideToSearch = slide;
+            }
+        }
+    })
+
+    return slideToSearch;
+}
+
+//Function parse string of pixels to number
+function leftStyleParse(value) {
+
+    return value === undefined ?
+        undefined :
+        Number.parseInt(value.substring(0, value.length - 2));
+}
+
+
+
+//Function checks form validation and shows info submit window.
 function SubmitForm() {
     if (!InputFormValidation()) {
         return;
@@ -136,17 +187,16 @@ function SubmitForm() {
     submitForms[1].innerText = theme;
     submitForms[2].innerText = description;
 
-    console.log(theme);
-    console.log(description);
-
     event.preventDefault();
     document.querySelector(".submit-info").style.display = "block";
 }
 
+//Function hide window after Ok click.
 function ConfirmSubmitOk() {
     document.querySelector(".submit-info").style.display = "none";
 }
 
+//Function returns true if name and email validation - Ok, otherwiese - false.
 function InputFormValidation(form) {
     return document.querySelector("#name").checkValidity() && document.querySelector("#email").checkValidity();
 }
